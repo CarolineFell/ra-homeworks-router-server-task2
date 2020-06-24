c0b6d7060f0e5aca3a1555c86e195bf531d4cdd2
Gradt <carolinefell@yandex.ru>
2020-06-24 08:52:31 +0300

    Error fix

--- server.js
+++ server.js
@@ -19,25 +19,25 @@ router.get('/posts', async (ctx, next) => {
 });
 
 router.post('/posts', async(ctx, next) => {
-    const {id, content} = ctx.request.body;
+  const {id, content} = ctx.request.body;
 
-    if (id !== 0) {
-        posts = posts.map(o => o.id !== id ? o : {...o, content: content});
-        ctx.response.status = 204;
-        return;
-    }
-
-    posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+  if (id !== 0) {
+    posts = posts.map(o => o.id !== id ? o : {...o, content: content});
     ctx.response.status = 204;
+    return;
+  }
+
+  posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+  ctx.response.status = 204;
 });
 
 router.delete('/posts/:id', async(ctx, next) => {
-    const postId = Number(ctx.params.id);
-    const index = posts.findIndex(o => o.id === postId);
-    if (index !== -1) {
-        posts.splice(index, 1);
-    }
-    ctx.response.status = 204;
+  const postId = Number(ctx.params.id);
+  const index = posts.findIndex(o => o.id === postId);
+  if (index !== -1) {
+    posts.splice(index, 1);
+  }
+  ctx.response.status = 204;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
